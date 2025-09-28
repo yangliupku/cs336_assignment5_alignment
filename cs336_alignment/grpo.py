d9bd60cb438acbbf7b428be4c80ccd740dcfc702
Yang Liu <yhvhliuyang@gmail.com>
2025-09-28 19:18:42 -0400

    Refactor util functions

--- cs336_alignment/grpo.py
+++ cs336_alignment/grpo.py
@@ -1,11 +1,17 @@
 import random
 import torch
 import pathlib
-from transformers import AutoTokenizer, AutoModelForCausalLM, PreTrainedModel
 from vllm.model_executor import set_random_seed as vllm_set_random_seed
-from vllm import LLM, SamplingParams
 from cs336_alignment.drgrpo_grader import r1_zero_reward_fn
-from cs336_alignment.utils import load_jsonl
+from cs336_alignment.utils import (
+    load_jsonl,
+    load_base_model,
+    init_vllm,
+    load_policy_into_vllm_instance,
+    get_validation_accuracy,
+    get_prompt,
+    sampling_params,
+)
 from cs336_alignment.alignment_helpers import (
     tokentize_prompt_and_output,
     get_response_log_probs,
@@ -14,7 +20,6 @@ from cs336_alignment.alignment_helpers import (
     grpo_microbatch_train_step,
 )
 
-from unittest.mock import patch
 from torch.optim import AdamW
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -42,71 +47,14 @@ MAX_GRAD_NORM = 1.0
 
 device = torch.device("cuda:0")
 
-sampling_params = SamplingParams(
-    temperature=1.0,
-    top_p=1.0,
-    max_tokens=1024,
-    stop=["</answer>"],
-    include_stop_str_in_output=True,
-    min_tokens=4,
-)
-
-
-def get_prompt(question):
-    return f"""A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer. The reasoning process is enclosed within <think> </think> and answer is enclosed within <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>.
-  User: {question}
-  Assistant: <think>"""
-
 
 def set_all_seed():
     random.seed(0)
     vllm_set_random_seed(0)
 
 
-def load_base_model():
-    model = AutoModelForCausalLM.from_pretrained(
-        MODEL_PATH, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2"
-    ).to(device)
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-    return (model, tokenizer)
-
-
-def init_vllm():
-    world_size_patch = patch("torch.distributed.get_world_size", return_value=1)
-    profiling_patch = patch(
-        "vllm.worker.worker.Worker._assert_memory_footprint_increased_during_profiling",
-        return_value=None,
-    )
-    with world_size_patch, profiling_patch:
-        return LLM(
-            model=str(MODEL_PATH),
-            device=torch.device("cuda:1"),
-            dtype=torch.bfloat16,
-            enable_prefix_caching=True,
-            gpu_memory_utilization=0.85,
-        )
-
-
-def load_policy_into_vllm_instance(policy: PreTrainedModel, llm: LLM):
-    state_dict = policy.state_dict()
-    llm_model = llm.llm_engine.model_executor.driver_worker.model_runner.model
-    llm_model.load_weights(state_dict.items())
-
-
-def get_validation_accuracy(ds, llm):
-    prompts = [get_prompt(example["problem"]) for example in ds]
-    results = []
-    responses = llm.generate(prompts, sampling_params)
-    for i in range(len(ds)):
-        solution = ds[i]["solution"]
-        model_response = responses[i].outputs[0].text
-        r1_reward = r1_zero_reward_fn(model_response, solution)
-        results.append(r1_reward["reward"])
-    return sum(results) / len(results)
-
-
 set_all_seed()
-model, tokenizer = load_base_model()
+model, tokenizer = load_base_model(device)
 llm = init_vllm()
 opt = AdamW(model.parameters(), lr=LR, weight_decay=0, betas=(0.9, 0.95))
 
